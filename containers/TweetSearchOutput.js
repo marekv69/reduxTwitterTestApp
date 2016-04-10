dc32253da88c8c9dbd62c2b4ef7fd229f10703b2
Marek Vasik <vasik@upcmail.cz>
2016-04-10 19:18:39 +0200

    Removed TweetList component. The code was moved to TweetSearchOutput

--- containers/TweetSearchOutput.js
+++ containers/TweetSearchOutput.js
@@ -1,11 +1,22 @@
 import Component from 'react-pure-render/component';
 import React, {PropTypes} from 'react';
 import { connect } from 'react-redux';
-import {Iterable} from 'immutable';
-import TweetList from '../components/TweetList.react';
-import SearchBar from '../components/SearchBar.react';
-import Message from '../components/Message.react';
+import TweetListButtonGroup from '../components/TweetListButtonGroup.react';
+import TweetListFilterBar from '../components/TweetListFilterBar.react';
+import TweetListFilteringAndSortingInfo from '../components/TweetListFilteringAndSortingInfo.react';
+import TweetListTweetsOutput from '../components/TweetListTweetsOutput.react';
+import TweetListModalInfo from '../components/TweetListModalInfo.react.js';
 
+/**
+ * This container is used for showing 50 latest tweets of a user if gathering the tweets using Twitter API
+ * finished successfully
+ * It contains following components:
+ * <TweetListButtonGroup> - buttons for sorting and showing modal info
+ * <TweetListFilterBar> - component for filtering tweets
+ * <TweetListFilteringAndSortingInfo> - component for showing current filtering and sorting info
+ * <TweetListTweetsOutput> - component showing the tweets (filtered and sorted if filtering/sorting is applied)
+ * <TweetListModalInfo> - component showing modal window containing basic statistics concerning the tweets
+ */
 class TweetSearchOutput extends Component {
   constructor(props) {
     super(props)
@@ -15,12 +26,17 @@ class TweetSearchOutput extends Component {
     const { tweets, currentSortingProperty,
         currentSortingType, filterString, showModalInfo, dispatch} = this.props;
 
-    const tweetsSearchOutput = <TweetList tweets={tweets}
-                                      currentSortingProperty={currentSortingProperty}
-                                      currentSortingType={currentSortingType}
-                                      filterString={filterString}
-                                      showModalInfo={showModalInfo}
-                                      dispatch={dispatch}/>;
+    const tweetsSearchOutput =
+        <div className="tweet-list">
+          <TweetListButtonGroup dispatch={this.props.dispatch}/>
+          <TweetListFilterBar filterString={this.props.filterString} dispatch={this.props.dispatch}/>
+          <TweetListFilteringAndSortingInfo filterString={this.props.filterString} currentSortingProperty=
+          {this.props.currentSortingProperty} currentSortingType={this.props.currentSortingType}/>
+          <TweetListTweetsOutput currentSortingProperty={this.props.currentSortingProperty} currentSortingType=
+          {this.props.currentSortingType} filterString={this.props.filterString} tweets={this.props.tweets}/>
+          <TweetListModalInfo tweets={this.props.tweets} dispatch={this.props.dispatch}
+                          showModalInfo={this.props.showModalInfo}/>
+        </div>
 
     return (
           tweets ? tweetsSearchOutput : null
